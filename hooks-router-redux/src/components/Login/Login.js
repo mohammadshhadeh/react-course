ea12b128edc9bcd9187182d9fbb659d715c56ac1
Mohammad Shehadeh <mohammadzaidsh@outlook.com>
2022-03-12 19:05:12 +0300

    useEffect

--- hooks-router-redux/src/components/Login/Login.js
+++ hooks-router-redux/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -11,20 +11,22 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  useEffect(() => {
+    const isValid = enteredEmail.includes('@') && enteredPassword.trim().length > 6;
+    if (isValid !== formIsValid) setFormIsValid(isValid);
+  }, [
+      enteredEmail,
+      enteredPassword,
+      formIsValid
+    ]
+  );
+
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
-
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
-
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
   };
 
   const validateEmailHandler = () => {
