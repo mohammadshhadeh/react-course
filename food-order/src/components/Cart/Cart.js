dadea3ee30757dac8e38f5c02189a79ac6b2ba31
Mohammad Shehadeh <mohammadzaidsh@outlook.com>
2022-03-16 18:40:56 +0300

    context, useReducer, useEffect, useState, useRef, forwardRef, Provider

--- food-order/src/components/Cart/Cart.js
+++ food-order/src/components/Cart/Cart.js
@@ -1,26 +1,55 @@
-import Modal from '../UI/Modal';
-import classes from './Cart.module.css';
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 
 const Cart = (props) => {
+    const cartCtx = useContext(CartContext);
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+    const hasItems = !!cartCtx.items.length;
+
+    const cartItemRemoveHandler = (id) => {
+        cartCtx.removeItem(id);
+    };
+
+    const cartItemAddHandler = (item) => {
+        cartCtx.addItem(item);
+    };
+
     const cartItems = (
         <ul className={classes['cart-items']}>
-            {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
-                <li>{item.name}</li>
+            {cartCtx.items.map((item) => (
+                <CartItem
+                    key={item.id}
+                    name={item.name}
+                    amount={item.amount}
+                    price={item.price}
+                    // or use cartItemAddHandler.bind(null, item.id)
+                    onRemove={() => cartItemRemoveHandler(item.id)}
+                    // or use cartItemAddHandler.bind(null, item)
+                    onAdd={() => cartItemAddHandler(item)}
+                />
             ))}
         </ul>
     );
 
     return (
-        <Modal>
+        <Modal onHideCart={props.onHideCart}>
             <div>
                 {cartItems}
                 <div className={classes.total}>
                     <span>Total Amount</span>
-                    <span>35.62</span>
+                    <span>{totalAmount}</span>
                 </div>
                 <div className={classes.actions}>
-                    <button className={classes['button--alt']}>Close</button>
-                    <button className={classes.button}>Order</button>
+                    <button
+                        className={classes["button--alt"]}
+                        onClick={props.onHideCart}
+                    >
+                        Close
+                    </button>
+                    {hasItems && <button className={classes.button}>Order</button>}
                 </div>
             </div>
         </Modal>
