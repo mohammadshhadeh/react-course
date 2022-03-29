9b3cb0614dc38ffb3498c8cb323f0120b967c114
mohammadshhadeh <mohammadzaidsh@outlook.com>
2022-03-29 22:42:42 +0300

    custom hooks

---
+++ custom-hooks/src/components/ForwardCounter.js
@@ -0,0 +1,12 @@
+import Card from './Card';
+import useCounter from '../hooks/use-counter';
+
+const ForwardCounter = () => {
+  const counter = useCounter(
+    (prevCounter) => prevCounter + 1
+  );
+
+	return <Card>{counter}</Card>;
+};
+
+export default ForwardCounter;
